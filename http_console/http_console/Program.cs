cf7007763d363d7dba471e11f6dfb605efc03a22
Colloid <c011319546@edu.teu.ac.jp>
2020-01-13 22:26:15 +0900

    First commit

---
+++ http_console/http_console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace http_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
