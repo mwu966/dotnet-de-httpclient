764e5bb84017f9e780bcb47d4d2e28fb8eb83380
Colloid <c011319546@edu.teu.ac.jp>
2020-01-13 23:26:38 +0900

    basicでget/postを投げる基礎固め

--- http_console/Program.cs
+++ http_console/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text;
 
 namespace http_console
 {
     class Program
     {
-        static void Main(string[] args)
+       private static HttpClient httpClient = new HttpClient();
+        static async void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string pass = "";
+            string id = "";
+
+            string base64str = Convert.ToBase64String(Encoding.UTF8.GetBytes(id + ":" + pass));
+
+            var content = new StringContent("");
+
+            content.Headers.Add(@"Authorization", @"Basic "+ base64str);
+
+
+            var postTest = await httpClient.PostAsync(@"test",content);
+
+            var getTest = await httpClient.GetAsync(@"test");            
+
         }
+
+
     }
 }
